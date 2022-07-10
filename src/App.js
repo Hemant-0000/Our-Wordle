1dc436e1ac3e401016cbe0e055c5012cd9df89b7
Hemant <hrsmart2150@gmail.com>
2022-07-10 10:58:06 +0530

    Made the hierarchy of our app next we build board

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
 
 function App() {
   return (
     <div className="App">
-      {/* <nav>
+      {/* Navbar  */}
+      <nav>
         <h1>Wordle</h1>
-      </nav> */}
+      </nav>
+
+    {/* Grid Component  */}
+    <Board/>
+
+    {/* Keyboard Component  */}
+    <Keyboard/>
+
+
     </div>
   );
 }
