37e5817c986875322b1fd9d1cdf495fe98f59cc8
Hemant <hrsmart2150@gmail.com>
2022-07-10 11:22:10 +0530

    Now we have build our board compo next we'll build keyboard ⌨  compo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { createContext, useState } from 'react';
 import './App.css';
 import Board from './components/Board';
 import Keyboard from './components/Keyboard';
+import { boardDefault } from './Words';
+
+export const AppContext = createContext();
 
 function App() {
+  const [board, setBoard] = useState(boardDefault)
   return (
     <div className="App">
       {/* Navbar  */}
@@ -10,11 +15,12 @@ function App() {
         <h1>Wordle</h1>
       </nav>
 
-    {/* Grid Component  */}
-    <Board/>
-
-    {/* Keyboard Component  */}
-    <Keyboard/>
+      <AppContext.Provider value={{ board, setBoard }}>
+        {/* Grid Component  */}
+        <Board />
+        {/* Keyboard Component  */}
+        <Keyboard />
+      </AppContext.Provider>
 
 
     </div>
